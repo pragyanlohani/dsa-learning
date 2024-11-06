3712a79949525070c8ddba84f61edb87367ec7bb
Pragyanlohnai <pragyanlohani022@gmail.com>
2024-11-06 14:27:05 +0530

    dsa-learning

---
+++ week1/conditionalLoops.cpp
@@ -0,0 +1,63 @@
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main (){
+//     int n ;
+//     cout<< "enter the number" ;
+//     cin>> n;
+
+//     if(n>=18){
+//         cout<< "preson is vote" ;
+
+
+
+
+//     }else if(n=>25 && n<25){
+//         cout<< "the person is not vote4 " ;
+//     }else {
+//         cout<< "the person is not vote4 " ;
+//     }
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+
+// using namespace std;
+// int main(){
+
+// char ch;
+// cout<<"enter the character ";
+// cin>> ch;
+
+
+// if (ch>='a' && ch<= 'z'){
+//     cout<<"lowercase" ;
+// }else{
+//     cout<<"uppercase" ;
+// }
+
+// return 0;
+// }
+
+// print no from 1 to 5 
+
+
+// #include<iostream>
+
+
+// using namespace std;
+
+// int main(){
+
+//     int count =1;
+
+//     while (count <=5){
+//         cout<<count ;
+//         count++;
+//     }
+//  return 0;
+// }
